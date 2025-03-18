bb8567f40dbd51a65dd773c018aad90f69c028f1
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2025-03-18 21:26:08 +0200

    Add saving chosen language in LocalStorage

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -1,18 +1,38 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import { useTranslation } from "react-i18next";
 import { LanguagesEnum } from "../../i18n/LanguagesEnum.ts";
 
 import "./LanguageSelector.css";
+import { useEffect } from "react";
 
 export const LanguageSelector = () => {
   const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem("language");
+    if (storedLanguage) {
+      i18n.changeLanguage(storedLanguage as LanguagesEnum);
+    }
+  }, [i18n]);
+
+  const handleLanguageChange = (ev: SelectChangeEvent) => {
+    const selectedLanguage = ev.target.value as LanguagesEnum;
+    i18n.changeLanguage(selectedLanguage);
+    localStorage.setItem("language", selectedLanguage);
+  };
 
   return (
     <FormControl className="languageForm" variant="standard">
       <InputLabel id="language-select-label">{t("language")}</InputLabel>
       <Select
         value={i18n.language}
-        onChange={(ev) => i18n.changeLanguage(ev.target.value as LanguagesEnum)}
+        onChange={handleLanguageChange}
         name="language"
         labelId="language-select-label"
       >
