10dc1cca540302c595664e1f4470f77774527ed1
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2025-03-26 14:30:30 +0200

    Fix UniversalModal

--- src/components/UniversalModal/UniversalModal.tsx
+++ src/components/UniversalModal/UniversalModal.tsx
@@ -1,42 +1,39 @@
-import React from "react";
+import { ReactNode } from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 
 import "./UniversalModal.css";
 
 interface UniversalModalProps {
-  buttonText: string;
+  open: boolean;
+  handleClose: () => void;
   modalTitle: string;
   modalText: string;
+  children?: ReactNode;
 }
 
 export const UniversalModal = ({
-  buttonText,
+  open,
+  handleClose,
   modalTitle,
   modalText,
+  children,
 }: UniversalModalProps) => {
-  const [open, setOpen] = React.useState<boolean>(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   return (
-    <div>
-      <Button onClick={handleOpen}>{buttonText}</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box className="modalBox">
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            {modalTitle}
-          </Typography>
-          <Typography id="modal-modal-description">{modalText}</Typography>
-        </Box>
-      </Modal>
-    </div>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box className="modalBox">
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          {modalTitle}
+        </Typography>
+        <Typography id="modal-modal-description">{modalText}</Typography>
+        <Box>{children}</Box>
+      </Box>
+    </Modal>
   );
 };
